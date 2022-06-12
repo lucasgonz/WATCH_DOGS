00ba0300ad79bdcad06b349062bca0111537aa91
LucasG <lucas.gonzalez.uqac@gmail.com>
2022-06-12 17:27:17 -0400

    Code - Add Configuration object

---
+++ Application/droneConfig.ts
@@ -0,0 +1,8 @@
+export const config = {
+   // number of % you loose every rate
+   dischargeRate: 4,
+   // number of seconcds every time discharge rate is apply
+   timeRate: 1,
+   // minimum charge for drone to be available number out of 100
+   minCharge: 10,
+}
